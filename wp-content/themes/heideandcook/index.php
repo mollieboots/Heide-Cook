3524ca3545fbf7a0804619c8746083e4772f815e
mollieboots <mollie.bootsma@gmail.com>
2016-06-06 13:52:02 -0700

    add custom heide and cook color palette from mockups

--- wp-content/themes/heideandcook/index.php
+++ wp-content/themes/heideandcook/index.php
@@ -2,4 +2,4 @@
 require_once __DIR__.'/App/bootstrap.php';
 
 $twig = $container->get("twig.environment");
-echo $twig->render('basic.html.twig');
+echo $twig->render('basic.html.twig', array('title' => get_the_title()));
