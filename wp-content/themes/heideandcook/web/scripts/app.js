ce52a7f37add8567090caef9dd0bebb320e9c1b6
mollieboots <mollie.bootsma@gmail.com>
2016-06-08 14:43:21 -0700

    resolve slick implementation issue

--- wp-content/themes/heideandcook/web/scripts/app.js
+++ wp-content/themes/heideandcook/web/scripts/app.js
@@ -1,7 +1,7 @@
 jQuery(document).foundation();
 console.log('im working');
-$(document).ready(function(){
-  $('.recent-work-slider').slick({
+jQuery(document).ready(function(){
+  jQuery('.recent-work-slider').slick({
     infinite: true,
     slidesToShow: 4,
     slidesToScroll: 1,
