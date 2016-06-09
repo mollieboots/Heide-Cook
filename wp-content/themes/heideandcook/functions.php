0a8c5c790443867e9546aa344f6c90d2b292041d
mollieboots <mollie.bootsma@gmail.com>
2016-06-09 10:43:55 -0700

    implement fancybox to index lightbox, link to video but it doesn't play

--- wp-content/themes/heideandcook/functions.php
+++ wp-content/themes/heideandcook/functions.php
@@ -73,12 +73,13 @@ add_action('wp_enqueue_scripts', function() {
     wp_register_style( 'Montserrat', 'https://fonts.googleapis.com/css?family=Montserrat:400,700' );
     wp_register_style( 'slick-css', get_template_directory_uri() . '/src/slick/slick/slick.css' );
     wp_register_style( 'slick-theme-css', get_template_directory_uri() . '/src/slick/slick/slick-theme.css' );
-    wp_register_style( 'app', get_template_directory_uri() . '/web/stylesheets/app.css',  ['Cabin', 'Raleway', 'Montserrat', 'slick-css', 'slick-theme-css'] );
+    wp_register_style( 'fancybox-css', get_template_directory_uri() . '/src/fancybox/source/jquery.fancybox.css' );
+    wp_register_style( 'app', get_template_directory_uri() . '/web/stylesheets/app.css',  ['Cabin', 'Raleway', 'Montserrat', 'slick-css', 'slick-theme-css', 'fancybox-css'] );
     wp_register_script( 'slick', get_template_directory_uri() . '/src/slick/slick/slick.js', ['jquery'] );
-    // wp_register_script( 'fancybox', get_template_directory_uri() . 'src/fancybox', array('jquery') );
+    wp_register_script( 'fancybox', get_template_directory_uri() . '/src/fancybox/source/jquery.fancybox.pack.js', ['jquery'] );
     wp_register_script( 'app_script', get_template_directory_uri() . '/web/scripts-min/app.min.js', ['slick'] );
     wp_enqueue_style( 'app' );
     wp_enqueue_script( 'slick' );
-    // wp_enqueue_script( 'fancybox' );
+    wp_enqueue_script( 'fancybox' );
     wp_enqueue_script( 'app_script' );
 });
--- wp-content/themes/heideandcook/web/scripts/app.js
+++ wp-content/themes/heideandcook/web/scripts/app.js
@@ -1,11 +1,17 @@
 jQuery(document).foundation();
-console.log('im working');
+
 jQuery(document).ready(function(){
   jQuery('.recent-work-slider').slick({
     infinite: true,
     slidesToShow: 4,
-    slidesToScroll: 2,
-    centerMode: true,
+    slidesToScroll: 1,
     arrows: true
   });
+
+  jQuery(".fancybox").fancybox();
+    openEffect  : 'fade',
+    closeEffect : 'fade',
+    helpers: {
+            media: {}
+        }
 });
