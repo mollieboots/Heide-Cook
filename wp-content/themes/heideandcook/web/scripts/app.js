2e49499188f02339cfe5587fed87cbaf67366166
mollieboots <mollie.bootsma@gmail.com>
2016-06-14 14:23:45 -0700

    successfully merge and fix merge  error

--- wp-content/themes/heideandcook/web/scripts/app.js
+++ wp-content/themes/heideandcook/web/scripts/app.js
@@ -5,7 +5,31 @@ jQuery(document).ready(function(){
     infinite: true,
     slidesToShow: 4,
     slidesToScroll: 1,
-    arrows: true
+    arrows: true,
+    responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+  }]
   });
 
   jQuery(".fancybox").fancybox();
