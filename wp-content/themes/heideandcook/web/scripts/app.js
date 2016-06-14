85efa03fa5fa6927fbcdec4a31752802e26e8d59
mollieboots <mollie.bootsma@gmail.com>
2016-06-14 16:07:02 -0700

    start blog page

--- wp-content/themes/heideandcook/web/scripts/app.js
+++ wp-content/themes/heideandcook/web/scripts/app.js
@@ -6,6 +6,7 @@ jQuery(document).ready(function(){
     slidesToShow: 4,
     slidesToScroll: 1,
     arrows: true,
+    dots: false,
     responsive: [
     {
       breakpoint: 1024,
@@ -13,21 +14,23 @@ jQuery(document).ready(function(){
         slidesToShow: 3,
         slidesToScroll: 1,
         infinite: true,
-        dots: true
+        dots: false
       }
     },
     {
       breakpoint: 600,
       settings: {
         slidesToShow: 2,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        dots: false
       }
     },
     {
       breakpoint: 480,
       settings: {
         slidesToShow: 1,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        dots: false
       }
   }]
   });
