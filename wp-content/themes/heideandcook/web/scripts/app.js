849479d2d2cc0434bb9bedd50e70ed0c581802d5
mollieboots <mollie.bootsma@gmail.com>
2016-06-30 17:24:53 -0700

    get quote form formmatted properly

--- wp-content/themes/heideandcook/web/scripts/app.js
+++ wp-content/themes/heideandcook/web/scripts/app.js
@@ -1,31 +1,80 @@
 jQuery(document).ready(function(){
   jQuery(document).foundation();
 
-  var megaMenu = jQuery('#mega-menu');
-  var megaMenuLink = jQuery('.services.menu-item a');
-  megaMenuLink.on('click', function(){
-    megaMenuLink.parent().toggleClass('active');
-    megaMenu.stop(true, true).fadeToggle();
+  jQuery('.services.menu-item a').on('click', function(){
+    jQuery('#mega-menu').slideToggle();
   });
 
   jQuery('.recent-work-slider').slick({
     infinite: true,
-    arrows: true,
-    dots: false,
     slidesToShow: 4,
     slidesToScroll: 1,
-    responsive: [{
-        breakpoint: 700,
-        settings: {
-          slidesToShow: 1
-        }
-      }, {
+    arrows: true,
+    dots: false,
+    responsive: [
+    {
       breakpoint: 1024,
       settings: {
-        slidesToShow: 2
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: false
       }
-    }]
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: false
+      }
+  }]
   });
 
-  jQuery(".fancybox").fancybox();
+
+// jQuery('.fancybox').fancybox({
+//        helpers: {
+//            title: {
+//                type: 'outside',
+//                position: 'top',
+//            },
+//            overlay: {
+//                css: {
+//                    'background' : 'rgba(255, 255, 255, 0.9)'
+//                }
+//            }
+//        },
+//        tpl: {
+//            closeBtn: '<a title="Close" class="fancybox-item fancybox-close my-close" href="javascript:;"></a>',
+//            next: '<a title="Next" class="fancybox-nav fancybox-next my-next" href="javascript:;"><span></span></a>',
+//            prev: '<a title="Previous" class="fancybox-nav fancybox-prev my-prev" href="javascript:;"><span></span></a>'
+//        },
+//        padding: 0,
+//    });
+
+  jQuery(".fancybox").fancybox({
+    helpers: {
+        title: {
+            type: 'outside',
+            position: 'top',
+        },
+    },
+    tpl: {
+      closeBtn : '<a title="Close" class="fancybox-item fancybox-close custom-close" href="javascript:;"></a>',
+    },
+    "scrolling" : "no",
+    "minWidth" : "610px",
+  });
 });
+
+if (jQuery('.accordion-title').attr('aria-expanded') === "true") {
+  console.log("open");
+}
