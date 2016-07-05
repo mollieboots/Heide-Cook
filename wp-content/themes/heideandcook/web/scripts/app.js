7686fb9176e867b9ee5e0504c5e7d479ba234426
mollieboots <mollie.bootsma@gmail.com>
2016-07-05 11:35:22 -0700

    remove unneeded slick slider formatting

--- wp-content/themes/heideandcook/web/scripts/app.js
+++ wp-content/themes/heideandcook/web/scripts/app.js
@@ -12,32 +12,6 @@ jQuery(document).ready(function(){
     slidesToScroll: 1,
     arrows: true,
     dots: false,
-    responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        infinite: true,
-        dots: false
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1,
-        dots: false
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        dots: false
-      }
-  }]
   });
 
   jQuery(".fancybox").fancybox({
